7d36e30cdcc9a5c6529746bdd657a513d7906938
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-26 13:00:20 -0700

    debugging

--- 2-largest_number.c
+++ 2-largest_number.c
@@ -8,22 +8,22 @@
 */
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
+	int largest;
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
